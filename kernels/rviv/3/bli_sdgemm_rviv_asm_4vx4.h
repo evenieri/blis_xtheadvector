daaf15e0f43894d0fdb89d68e93742b33212db34
evenieri <emanuelevenieri@gmail.com>
2024-09-27 10:22:12 +0200

    Updated files in order to be compiled for XTheadVector extension of Milk-V Pioneer

--- kernels/rviv/3/bli_sdgemm_rviv_asm_4vx4.h
+++ kernels/rviv/3/bli_sdgemm_rviv_asm_4vx4.h
@@ -140,7 +140,7 @@
 REALNAME:
 	#include "rviv_save_registers.h"
 
-	vsetvli s0, zero, VTYPE, m1, ta, ma
+	th.vsetvli s0, zero, VTYPE, m1
 	csrr s0, vlenb
 	FZERO(fzero)
 
@@ -154,22 +154,22 @@ REALNAME:
 	add C13_ptr, C03_ptr, rs_c
 
 	// Zero-initialize accumulators
-	vxor.vv AB00, AB00, AB00
-	vxor.vv AB01, AB01, AB01
-	vxor.vv AB02, AB02, AB02
-	vxor.vv AB03, AB03, AB03
-	vxor.vv AB10, AB10, AB10
-	vxor.vv AB11, AB11, AB11
-	vxor.vv AB12, AB12, AB12
-	vxor.vv AB13, AB13, AB13
-	vxor.vv AB20, AB20, AB20
-	vxor.vv AB21, AB21, AB21
-	vxor.vv AB22, AB22, AB22
-	vxor.vv AB23, AB23, AB23
-	vxor.vv AB30, AB30, AB30
-	vxor.vv AB31, AB31, AB31
-	vxor.vv AB32, AB32, AB32
-	vxor.vv AB33, AB33, AB33
+	th.vxor.vv AB00, AB00, AB00
+	th.vxor.vv AB01, AB01, AB01
+	th.vxor.vv AB02, AB02, AB02
+	th.vxor.vv AB03, AB03, AB03
+	th.vxor.vv AB10, AB10, AB10
+	th.vxor.vv AB11, AB11, AB11
+	th.vxor.vv AB12, AB12, AB12
+	th.vxor.vv AB13, AB13, AB13
+	th.vxor.vv AB20, AB20, AB20
+	th.vxor.vv AB21, AB21, AB21
+	th.vxor.vv AB22, AB22, AB22
+	th.vxor.vv AB23, AB23, AB23
+	th.vxor.vv AB30, AB30, AB30
+	th.vxor.vv AB31, AB31, AB31
+	th.vxor.vv AB32, AB32, AB32
+	th.vxor.vv AB33, AB33, AB33
 
 	// Handle k == 0
 	beqz loop_counter, MULTIPLYBETA
@@ -206,15 +206,15 @@ REALNAME:
 LOOP_UNROLL_4:
 	addi loop_counter, loop_counter, -4
 
-	vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
-	vfmacc.vf AB01, B01, A00
-	vfmacc.vf AB02, B02, A00
-	vfmacc.vf AB03, B03, A00
+	th.vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
+	th.vfmacc.vf AB01, B01, A00
+	th.vfmacc.vf AB02, B02, A00
+	th.vfmacc.vf AB03, B03, A00
 
-	vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
-	vfmacc.vf AB11, B01, A10
-	vfmacc.vf AB12, B02, A10
-	vfmacc.vf AB13, B03, A10
+	th.vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
+	th.vfmacc.vf AB11, B01, A10
+	th.vfmacc.vf AB12, B02, A10
+	th.vfmacc.vf AB13, B03, A10
 
 	// Load B(l+1,0:3)
 	FLOAD B10, 4*DATASIZE(B_row_ptr)
@@ -223,10 +223,10 @@ LOOP_UNROLL_4:
 	FLOAD B13, 7*DATASIZE(B_row_ptr)
 	addi B_row_ptr, B_row_ptr, 8*DATASIZE
 
-	vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
-	vfmacc.vf AB21, B01, A20
-	vfmacc.vf AB22, B02, A20
-	vfmacc.vf AB23, B03, A20
+	th.vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
+	th.vfmacc.vf AB21, B01, A20
+	th.vfmacc.vf AB22, B02, A20
+	th.vfmacc.vf AB23, B03, A20
 
 	// Load A(:,l+1)
 	VLE A01, (A01_ptr)
@@ -240,15 +240,15 @@ LOOP_UNROLL_4:
 	add A20_ptr, A21_ptr, s0
 	add A30_ptr, A31_ptr, s0
 
-	vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
-	vfmacc.vf AB31, B01, A30
-	vfmacc.vf AB32, B02, A30
-	vfmacc.vf AB33, B03, A30
+	th.vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
+	th.vfmacc.vf AB31, B01, A30
+	th.vfmacc.vf AB32, B02, A30
+	th.vfmacc.vf AB33, B03, A30
 
-	vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,1) * B(1,:)
-	vfmacc.vf AB01, B11, A01
-	vfmacc.vf AB02, B12, A01
-	vfmacc.vf AB03, B13, A01
+	th.vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,1) * B(1,:)
+	th.vfmacc.vf AB01, B11, A01
+	th.vfmacc.vf AB02, B12, A01
+	th.vfmacc.vf AB03, B13, A01
 
 	// Load B(l+2,0:3)
 	FLOAD B00, 0*DATASIZE(B_row_ptr)
@@ -256,10 +256,10 @@ LOOP_UNROLL_4:
 	FLOAD B02, 2*DATASIZE(B_row_ptr)
 	FLOAD B03, 3*DATASIZE(B_row_ptr)
 
-	vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,1) * B(1,:)
-	vfmacc.vf AB11, B11, A11
-	vfmacc.vf AB12, B12, A11
-	vfmacc.vf AB13, B13, A11
+	th.vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,1) * B(1,:)
+	th.vfmacc.vf AB11, B11, A11
+	th.vfmacc.vf AB12, B12, A11
+	th.vfmacc.vf AB13, B13, A11
 
 	// Load A(:,l+2)
 	VLE A00, (A00_ptr)
@@ -273,15 +273,15 @@ LOOP_UNROLL_4:
 	add A21_ptr, A20_ptr, s0
 	add A31_ptr, A30_ptr, s0
 
-	vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,1) * B(1,:)
-	vfmacc.vf AB21, B11, A21
-	vfmacc.vf AB22, B12, A21
-	vfmacc.vf AB23, B13, A21
+	th.vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,1) * B(1,:)
+	th.vfmacc.vf AB21, B11, A21
+	th.vfmacc.vf AB22, B12, A21
+	th.vfmacc.vf AB23, B13, A21
 
-	vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,1) * B(1,:)
-	vfmacc.vf AB31, B11, A31
-	vfmacc.vf AB32, B12, A31
-	vfmacc.vf AB33, B13, A31
+	th.vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,1) * B(1,:)
+	th.vfmacc.vf AB31, B11, A31
+	th.vfmacc.vf AB32, B12, A31
+	th.vfmacc.vf AB33, B13, A31
 
 	// Load A(:,l+3)
 	VLE A01, (A01_ptr)
@@ -295,10 +295,10 @@ LOOP_UNROLL_4:
 	add A20_ptr, A21_ptr, s0
 	add A30_ptr, A31_ptr, s0
 
-	vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,2) * B(2,:)
-	vfmacc.vf AB01, B01, A00
-	vfmacc.vf AB02, B02, A00
-	vfmacc.vf AB03, B03, A00
+	th.vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,2) * B(2,:)
+	th.vfmacc.vf AB01, B01, A00
+	th.vfmacc.vf AB02, B02, A00
+	th.vfmacc.vf AB03, B03, A00
 
 	// Load B(l+3,0:3)
 	FLOAD B10, 4*DATASIZE(B_row_ptr)
@@ -307,40 +307,40 @@ LOOP_UNROLL_4:
 	FLOAD B13, 7*DATASIZE(B_row_ptr)
 	addi B_row_ptr, B_row_ptr, 8*DATASIZE
 
-	vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,2) * B(2,:)
-	vfmacc.vf AB11, B01, A10
-	vfmacc.vf AB12, B02, A10
-	vfmacc.vf AB13, B03, A10
+	th.vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,2) * B(2,:)
+	th.vfmacc.vf AB11, B01, A10
+	th.vfmacc.vf AB12, B02, A10
+	th.vfmacc.vf AB13, B03, A10
 
-	vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,2) * B(2,:)
-	vfmacc.vf AB21, B01, A20
-	vfmacc.vf AB22, B02, A20
-	vfmacc.vf AB23, B03, A20
+	th.vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,2) * B(2,:)
+	th.vfmacc.vf AB21, B01, A20
+	th.vfmacc.vf AB22, B02, A20
+	th.vfmacc.vf AB23, B03, A20
 
-	vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,2) * B(3,:)
-	vfmacc.vf AB31, B01, A30
-	vfmacc.vf AB32, B02, A30
-	vfmacc.vf AB33, B03, A30
+	th.vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,2) * B(3,:)
+	th.vfmacc.vf AB31, B01, A30
+	th.vfmacc.vf AB32, B02, A30
+	th.vfmacc.vf AB33, B03, A30
 
-	vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,3) * B(3,:)
-	vfmacc.vf AB01, B11, A01
-	vfmacc.vf AB02, B12, A01
-	vfmacc.vf AB03, B13, A01
+	th.vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,3) * B(3,:)
+	th.vfmacc.vf AB01, B11, A01
+	th.vfmacc.vf AB02, B12, A01
+	th.vfmacc.vf AB03, B13, A01
 
-	vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,3) * B(3,:)
-	vfmacc.vf AB11, B11, A11
-	vfmacc.vf AB12, B12, A11
-	vfmacc.vf AB13, B13, A11
+	th.vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,3) * B(3,:)
+	th.vfmacc.vf AB11, B11, A11
+	th.vfmacc.vf AB12, B12, A11
+	th.vfmacc.vf AB13, B13, A11
 
-	vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,3) * B(3,:)
-	vfmacc.vf AB21, B11, A21
-	vfmacc.vf AB22, B12, A21
-	vfmacc.vf AB23, B13, A21
+	th.vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,3) * B(3,:)
+	th.vfmacc.vf AB21, B11, A21
+	th.vfmacc.vf AB22, B12, A21
+	th.vfmacc.vf AB23, B13, A21
 
-	vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,3) * B(3,:)
-	vfmacc.vf AB31, B11, A31
-	vfmacc.vf AB32, B12, A31
-	vfmacc.vf AB33, B13, A31
+	th.vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,3) * B(3,:)
+	th.vfmacc.vf AB31, B11, A31
+	th.vfmacc.vf AB32, B12, A31
+	th.vfmacc.vf AB33, B13, A31
 
 	li tmp, 3
 	ble loop_counter, tmp, TAIL_UNROLL_2
@@ -388,19 +388,19 @@ TAIL_UNROLL_2: // loop_counter <= 3
 	add A21_ptr, A20_ptr, s0
 	add A31_ptr, A30_ptr, s0
 
-	vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
-	vfmacc.vf AB01, B01, A00
-	vfmacc.vf AB02, B02, A00
-	vfmacc.vf AB03, B03, A00
+	th.vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
+	th.vfmacc.vf AB01, B01, A00
+	th.vfmacc.vf AB02, B02, A00
+	th.vfmacc.vf AB03, B03, A00
 
 	// Load A(2:3,l)
 	VLE A20, (A20_ptr)
 	VLE A30, (A30_ptr)
 
-	vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
-	vfmacc.vf AB11, B01, A10
-	vfmacc.vf AB12, B02, A10
-	vfmacc.vf AB13, B03, A10
+	th.vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
+	th.vfmacc.vf AB11, B01, A10
+	th.vfmacc.vf AB12, B02, A10
+	th.vfmacc.vf AB13, B03, A10
 
 	// Load B(l+1,0:3)
 	FLOAD B10, 4*DATASIZE(B_row_ptr)
@@ -415,15 +415,15 @@ TAIL_UNROLL_2: // loop_counter <= 3
 	VLE A21, (A21_ptr)
 	VLE A31, (A31_ptr)
 
-	vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
-	vfmacc.vf AB21, B01, A20
-	vfmacc.vf AB22, B02, A20
-	vfmacc.vf AB23, B03, A20
+	th.vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
+	th.vfmacc.vf AB21, B01, A20
+	th.vfmacc.vf AB22, B02, A20
+	th.vfmacc.vf AB23, B03, A20
 
-	vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
-	vfmacc.vf AB31, B01, A30
-	vfmacc.vf AB32, B02, A30
-	vfmacc.vf AB33, B03, A30
+	th.vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
+	th.vfmacc.vf AB31, B01, A30
+	th.vfmacc.vf AB32, B02, A30
+	th.vfmacc.vf AB33, B03, A30
 
 	// Point to A(:,l+2)
 	add A00_ptr, A01_ptr, s0
@@ -431,25 +431,25 @@ TAIL_UNROLL_2: // loop_counter <= 3
 	add A20_ptr, A21_ptr, s0
 	add A30_ptr, A31_ptr, s0
 
-	vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,1) * B(1,:)
-	vfmacc.vf AB01, B11, A01
-	vfmacc.vf AB02, B12, A01
-	vfmacc.vf AB03, B13, A01
+	th.vfmacc.vf AB00, B10, A01   // AB(0,:) += A(0,1) * B(1,:)
+	th.vfmacc.vf AB01, B11, A01
+	th.vfmacc.vf AB02, B12, A01
+	th.vfmacc.vf AB03, B13, A01
 
-	vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,1) * B(1,:)
-	vfmacc.vf AB11, B11, A11
-	vfmacc.vf AB12, B12, A11
-	vfmacc.vf AB13, B13, A11
+	th.vfmacc.vf AB10, B10, A11   // AB(1,:) += A(1,1) * B(1,:)
+	th.vfmacc.vf AB11, B11, A11
+	th.vfmacc.vf AB12, B12, A11
+	th.vfmacc.vf AB13, B13, A11
 
-	vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,1) * B(1,:)
-	vfmacc.vf AB21, B11, A21
-	vfmacc.vf AB22, B12, A21
-	vfmacc.vf AB23, B13, A21
+	th.vfmacc.vf AB20, B10, A21   // AB(2,:) += A(2,1) * B(1,:)
+	th.vfmacc.vf AB21, B11, A21
+	th.vfmacc.vf AB22, B12, A21
+	th.vfmacc.vf AB23, B13, A21
 
-	vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,1) * B(1,:)
-	vfmacc.vf AB31, B11, A31
-	vfmacc.vf AB32, B12, A31
-	vfmacc.vf AB33, B13, A31
+	th.vfmacc.vf AB30, B10, A31   // AB(3,:) += A(3,1) * B(1,:)
+	th.vfmacc.vf AB31, B11, A31
+	th.vfmacc.vf AB32, B12, A31
+	th.vfmacc.vf AB33, B13, A31
 
 	li tmp, 1
 	ble loop_counter, tmp, TAIL_UNROLL_1
@@ -469,49 +469,49 @@ TAIL_UNROLL_1: // loop_counter <= 1
 	VLE A20, (A20_ptr)
 	VLE A30, (A30_ptr)
 
-	vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
-	vfmacc.vf AB01, B01, A00
-	vfmacc.vf AB02, B02, A00
-	vfmacc.vf AB03, B03, A00
+	th.vfmacc.vf AB00, B00, A00   // AB(0,:) += A(0,0) * B(0,:)
+	th.vfmacc.vf AB01, B01, A00
+	th.vfmacc.vf AB02, B02, A00
+	th.vfmacc.vf AB03, B03, A00
 
-	vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
-	vfmacc.vf AB11, B01, A10
-	vfmacc.vf AB12, B02, A10
-	vfmacc.vf AB13, B03, A10
+	th.vfmacc.vf AB10, B00, A10   // AB(1,:) += A(1,0) * B(0,:)
+	th.vfmacc.vf AB11, B01, A10
+	th.vfmacc.vf AB12, B02, A10
+	th.vfmacc.vf AB13, B03, A10
 
-	vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
-	vfmacc.vf AB21, B01, A20
-	vfmacc.vf AB22, B02, A20
-	vfmacc.vf AB23, B03, A20
+	th.vfmacc.vf AB20, B00, A20   // AB(2,:) += A(2,0) * B(0,:)
+	th.vfmacc.vf AB21, B01, A20
+	th.vfmacc.vf AB22, B02, A20
+	th.vfmacc.vf AB23, B03, A20
 
-	vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
-	vfmacc.vf AB31, B01, A30
-	vfmacc.vf AB32, B02, A30
-	vfmacc.vf AB33, B03, A30
+	th.vfmacc.vf AB30, B00, A30   // AB(3,:) += A(3,0) * B(0,:)
+	th.vfmacc.vf AB31, B01, A30
+	th.vfmacc.vf AB32, B02, A30
+	th.vfmacc.vf AB33, B03, A30
 
 MULTIPLYALPHA:
 	FLOAD ALPHA, (a1)
 
 	// Multiply with alpha
-	vfmul.vf AB00, AB00, ALPHA
-	vfmul.vf AB01, AB01, ALPHA
-	vfmul.vf AB02, AB02, ALPHA
-	vfmul.vf AB03, AB03, ALPHA
+	th.vfmul.vf AB00, AB00, ALPHA
+	th.vfmul.vf AB01, AB01, ALPHA
+	th.vfmul.vf AB02, AB02, ALPHA
+	th.vfmul.vf AB03, AB03, ALPHA
 
-	vfmul.vf AB10, AB10, ALPHA
-	vfmul.vf AB11, AB11, ALPHA
-	vfmul.vf AB12, AB12, ALPHA
-	vfmul.vf AB13, AB13, ALPHA
+	th.vfmul.vf AB10, AB10, ALPHA
+	th.vfmul.vf AB11, AB11, ALPHA
+	th.vfmul.vf AB12, AB12, ALPHA
+	th.vfmul.vf AB13, AB13, ALPHA
 
-	vfmul.vf AB20, AB20, ALPHA
-	vfmul.vf AB21, AB21, ALPHA
-	vfmul.vf AB22, AB22, ALPHA
-	vfmul.vf AB23, AB23, ALPHA
+	th.vfmul.vf AB20, AB20, ALPHA
+	th.vfmul.vf AB21, AB21, ALPHA
+	th.vfmul.vf AB22, AB22, ALPHA
+	th.vfmul.vf AB23, AB23, ALPHA
 
-	vfmul.vf AB30, AB30, ALPHA
-	vfmul.vf AB31, AB31, ALPHA
-	vfmul.vf AB32, AB32, ALPHA
-	vfmul.vf AB33, AB33, ALPHA
+	th.vfmul.vf AB30, AB30, ALPHA
+	th.vfmul.vf AB31, AB31, ALPHA
+	th.vfmul.vf AB32, AB32, ALPHA
+	th.vfmul.vf AB33, AB33, ALPHA
 
 MULTIPLYBETA:
 	FLOAD BETA,  (a4)
@@ -557,10 +557,10 @@ BETANOTZERO:
 	VLE C02, (C02_ptr)
 	VLE C03, (C03_ptr)
 
-	vfmacc.vf AB00, BETA, C00
-	vfmacc.vf AB01, BETA, C01
-	vfmacc.vf AB02, BETA, C02
-	vfmacc.vf AB03, BETA, C03
+	th.vfmacc.vf AB00, BETA, C00
+	th.vfmacc.vf AB01, BETA, C01
+	th.vfmacc.vf AB02, BETA, C02
+	th.vfmacc.vf AB03, BETA, C03
 
 	VSE AB00, (C00_ptr)  // Store C(0:VLEN-1, 0:3)
 	VSE AB01, (C01_ptr)
@@ -577,10 +577,10 @@ BETANOTZERO:
 	VLE C12, (C12_ptr)
 	VLE C13, (C13_ptr)
 
-	vfmacc.vf AB10, BETA, C10
-	vfmacc.vf AB11, BETA, C11
-	vfmacc.vf AB12, BETA, C12
-	vfmacc.vf AB13, BETA, C13
+	th.vfmacc.vf AB10, BETA, C10
+	th.vfmacc.vf AB11, BETA, C11
+	th.vfmacc.vf AB12, BETA, C12
+	th.vfmacc.vf AB13, BETA, C13
 
 	VSE AB10, (C10_ptr)  // Store C(VLEN:2*VLEN-1, 0:3)
 	VSE AB11, (C11_ptr)
@@ -597,10 +597,10 @@ BETANOTZERO:
 	VLE C22, (C02_ptr)
 	VLE C23, (C03_ptr)
 
-	vfmacc.vf AB20, BETA, C20
-	vfmacc.vf AB21, BETA, C21
-	vfmacc.vf AB22, BETA, C22
-	vfmacc.vf AB23, BETA, C23
+	th.vfmacc.vf AB20, BETA, C20
+	th.vfmacc.vf AB21, BETA, C21
+	th.vfmacc.vf AB22, BETA, C22
+	th.vfmacc.vf AB23, BETA, C23
 
 	VSE AB20, (C00_ptr)  // Store C(2*VLEN:3*VLEN-1, 0:3)
 	VSE AB21, (C01_ptr)
@@ -612,10 +612,10 @@ BETANOTZERO:
 	VLE C32, (C12_ptr)
 	VLE C33, (C13_ptr)
 
-	vfmacc.vf AB30, BETA, C30
-	vfmacc.vf AB31, BETA, C31
-	vfmacc.vf AB32, BETA, C32
-	vfmacc.vf AB33, BETA, C33
+	th.vfmacc.vf AB30, BETA, C30
+	th.vfmacc.vf AB31, BETA, C31
+	th.vfmacc.vf AB32, BETA, C32
+	th.vfmacc.vf AB33, BETA, C33
 
 	VSE AB30, (C10_ptr)  // Store C(3*VLEN:4*VLEN-1, 0:3)
 	VSE AB31, (C11_ptr)
